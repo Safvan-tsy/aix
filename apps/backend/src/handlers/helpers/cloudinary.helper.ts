3957e993d72db3730719dd4eb542e436f7e59f2c
Safvan-tsy <safvanmanikulath@gmail.com>
2024-06-16 11:47:52 +0530

    test:cloudinary

--- apps/backend/src/handlers/helpers/cloudinary.helper.ts
+++ apps/backend/src/handlers/helpers/cloudinary.helper.ts
@@ -2,18 +2,18 @@ import { CloudinaryUploadResponse } from "../../types/cloudinary";
 import { v2 as cloudinary } from "cloudinary";
 
 const uploadPdfToCloudinary = (
-    buffer: Buffer
-  ): Promise<CloudinaryUploadResponse> => {
-    return new Promise((resolve, reject) => {
-      const stream = cloudinary.uploader.upload_stream(
-        { folder: "resumes" },
-        (error, result) => {
-          if (error) return reject(error);
-          resolve(result as CloudinaryUploadResponse);
-        }
-      );
-      stream.end(buffer);
-    });
-  };
-  
-  export {uploadPdfToCloudinary}
+  buffer: Buffer
+): Promise<CloudinaryUploadResponse> => {
+  return new Promise((resolve, reject) => {
+    const stream = cloudinary.uploader.upload_stream(
+      { folder: "resumes" },
+      (error, result) => {
+        if (error) return reject(error);
+        resolve(result as CloudinaryUploadResponse);
+      }
+    );
+    stream.end(buffer);
+  });
+};
+
+export { uploadPdfToCloudinary };
--- apps/backend/test/handlers/helpers/cloudinary.helper.test.ts
+++ apps/backend/test/handlers/helpers/cloudinary.helper.test.ts
@@ -1,5 +1,68 @@
 import { describe, it } from "vitest";
+import { CloudinaryUploadResponse } from "../../../src/types/cloudinary";
+import { v2 as cloudinary } from "cloudinary";
+import { uploadPdfToCloudinary } from "../../../src/handlers/helpers/cloudinary.helper";
+import { mockCloudinaryResponse } from "../mockData";
 
+beforeEach(() => {
+  vi.mock("cloudinary", () => ({
+    v2: {
+      uploader: {
+        upload_stream: vi.fn(),
+      },
+    },
+  }));
+});
+afterEach(() => {
+  vi.resetAllMocks();
+});
 describe("uploadPdfToCloudinary", () => {
-  it("", () => {});
+  it("should upload a PDF to Cloudinary and return the response", async () => {
+    const mockPDFBuffer = Buffer.from("PDF content");
+    const mockUploadStream = (
+      options: any,
+      callback: (error: any, result: any) => void
+    ) => {
+      return {
+        end: (buffer: Buffer) => {
+          callback(null, mockCloudinaryResponse);
+        },
+      };
+    };
+
+    (cloudinary.uploader.upload_stream as any).mockImplementation(
+      mockUploadStream
+    );
+
+    const response = await uploadPdfToCloudinary(mockPDFBuffer);
+
+    expect(response).toEqual(mockCloudinaryResponse);
+    expect(cloudinary.uploader.upload_stream).toHaveBeenCalledWith(
+      { folder: "resumes" },
+      expect.any(Function)
+    );
+  });
+  it("should reject if there is an error during upload", async () => {
+    const buffer = Buffer.from("fake pdf content");
+    const mockError = new Error("Upload failed");
+
+    const mockUploadStream = (
+      options: any,
+      callback: (error: any, result: any) => void
+    ) => {
+      return {
+        end: (buffer: Buffer) => {
+          callback(mockError, null);
+        },
+      };
+    };
+
+    (cloudinary.uploader.upload_stream as any).mockImplementation(
+      mockUploadStream
+    );
+
+    await expect(uploadPdfToCloudinary(buffer)).rejects.toThrow(
+      "Upload failed"
+    );
+  });
 });
